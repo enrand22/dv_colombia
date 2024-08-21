0e885181e324a0c5530cb4886bcd0f63de36b247
= <enriqueand22@outlook.com>
2024-08-21 12:30:01 -0600

    init

---
+++ lib/dv_colombia.ex
@@ -0,0 +1,37 @@
+defmodule DvColombia do
+  @factores [3, 7, 13, 17, 19, 23, 29, 37, 41, 43, 47, 53, 59, 67, 71]
+
+
+  @moduledoc """
+  Documentation for `DvColombia`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DvColombia.hello()
+      :world
+
+  """
+  def calc_nit(nit) do
+    factors = real_factors(nit)
+
+    nit
+    |> String.graphemes()
+    |> Enum.map(&(String.to_integer(&1)))
+    |> Enum.with_index(fn v, i -> v * Enum.at(factors, i) end)
+    |> Enum.sum()
+    |> Kernel.rem(11)
+    |> dv()
+  end
+
+  def dv(v) when v <= 1, do: v
+  def dv(v),             do: 11 - v
+
+  def real_factors(nit) do
+    length = nit |> String.length()
+    Enum.take(@factores, length) |> Enum.reverse
+  end
+end
---
+++ test/dv_colombia_test.exs
@@ -0,0 +1,8 @@
+defmodule DvColombiaTest do
+  use ExUnit.Case
+  doctest DvColombia
+
+  test "greets the world" do
+    assert DvColombia.hello() == :world
+  end
+end
